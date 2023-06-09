785431823e1a83ec72d70ac73d846229526c1737
Sandeepkumar2973 <106466194+Sandeepkumar2973@users.noreply.github.com>
2023-06-09 14:28:09 +0530

    comit final
    portfolio

---
+++ controller/sdmin.js
@@ -0,0 +1,4 @@
+const userModel = require('../models/userModel');
+
+
+export default getAllusersContrlr;
---
+++ react-portfolio-master/build/precache-manifest.8077b39f823384ac099ca1136134f9e6.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afca56d5c2ff8c2a555f069a6f2b4122",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93dd618883b39ea2e277",
+    "url": "/static/css/2.14351861.chunk.css"
+  },
+  {
+    "revision": "230e43e164113a943338",
+    "url": "/static/css/main.a8439a2d.chunk.css"
+  },
+  {
+    "revision": "93dd618883b39ea2e277",
+    "url": "/static/js/2.0740624b.chunk.js"
+  },
+  {
+    "revision": "b1f339f9d6fd28a415269e8f991a7374",
+    "url": "/static/js/2.0740624b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "230e43e164113a943338",
+    "url": "/static/js/main.27a3aefe.chunk.js"
+  },
+  {
+    "revision": "79d439c7be0dd499b205",
+    "url": "/static/js/runtime-main.e9933245.js"
+  },
+  {
+    "revision": "ce9b13e58fdd40e48635dd1de6aec90c",
+    "url": "/static/media/Hello.ce9b13e5.jpg"
+  },
+  {
+    "revision": "38828042bce3def5dbc3c5b4a5d2df93",
+    "url": "/static/media/Pencil.38828042.jpg"
+  },
+  {
+    "revision": "974fd9e5627ef4ef568718c3e5fe0124",
+    "url": "/static/media/p1.974fd9e5.jpeg"
+  },
+  {
+    "revision": "d4c96422b5dd318992f3912aba7f2a61",
+    "url": "/static/media/p3.d4c96422.png"
+  },
+  {
+    "revision": "01a89eb03c314b7132c34b6edb3ae216",
+    "url": "/static/media/p4.01a89eb0.png"
+  },
+  {
+    "revision": "88bc2a4c41ece14628ca18c9c411a083",
+    "url": "/static/media/p5.88bc2a4c.jpeg"
+  },
+  {
+    "revision": "dd47ff3a1ad60d0d5ea8d2dbaeadd642",
+    "url": "/static/media/sk.dd47ff3a.png"
+  }
+]);
